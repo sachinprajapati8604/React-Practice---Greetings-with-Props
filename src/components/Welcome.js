9b83cd85e4d09badf85fa8ef3b565c2c9f898df3
Sachin Prajapati <53299703+sachinprajapati8604@users.noreply.github.com>
2021-08-12 13:37:20 +0530

    updated

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,10 +1,13 @@
-function Welcome(prop) {
+// write code for Welcome component here
+import React, { Component } from "react";
+
+function Welcome(props) {
   return (
     <div>
-      <h1>Hey {prop.name} !</h1>
+      <h1>Hey {props.name}!</h1>
       <h2>Welcome to Newton School.</h2>
     </div>
   );
 }
-
 export default Welcome;
+
